941e48fa87b9cef96005fb717713a40b186e5428
gaabrielmooreira <gaabrielmooreira@gmail.com>
2022-11-22 15:42:17 -0300

    projeto jogoDaForca com requisito Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <header>
+        <h1>1 - ESTADO INICIAL DO JOGO</h1>
+      </header>
+
+      <div className="jogo">
+        <img src="assets/forca0.png" alt="Forca"/>
+        <div>
+          <button className="botao-iniciar">Escolher Palavra</button>
+          <p>_ _ _ _ _ _ _</p>
+        </div>
+      </div>
+
+      <div className="letras">
+        <button class="letra apagada">A</button>
+      </div>
+
+      <div className="chute">
+        <p>Já sei a palavra!</p>
+        <input type="text"></input>
+        <button>Chutar</button>
+      </div>
     </div>
   );
 }
